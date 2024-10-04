a37f26f602ab7aa8fd9591a0130162891dd5acf0
tesbro1194 <tes1194@naver.com>
2024-10-04 14:29:19 +0900

    오류가 나는 코드

--- src/main/java/com/sparta/planner/planner/lv4/service/PlanService.java
+++ src/main/java/com/sparta/planner/planner/lv4/service/PlanService.java
@@ -2,7 +2,10 @@ package com.sparta.planner.planner.lv4.service;
 
 import com.sparta.planner.planner.lv4.dto.PlanRequestDto;
 import com.sparta.planner.planner.lv4.dto.PlanResponseDto;
+import com.sparta.planner.planner.lv4.dto.UserRequestDto;
+import com.sparta.planner.planner.lv4.dto.UserResponseDto;
 import com.sparta.planner.planner.lv4.entity.Plan;
+import com.sparta.planner.planner.lv4.entity.User;
 import com.sparta.planner.planner.lv4.repository.PlanRepository;
 import org.springframework.stereotype.Service;
 import java.util.List;
@@ -30,7 +33,7 @@ public class PlanService {
         return new PlanResponseDto(plan);
     }
     public Long updatePlan(Long id, String pw, PlanRequestDto requestDto) {
-        Plan plan = planRepository.identify(id, pw);
+        Plan plan = planRepository.identifyForPlan(id, pw);
         if (plan != null) {
             planRepository.updatePlan(id, requestDto);
             return id;
@@ -39,12 +42,31 @@ public class PlanService {
         }
     }
     public Long delete(Long id, String pw) {
-        Plan plan = planRepository.identify(id, pw);
+        Plan plan = planRepository.identifyForPlan(id, pw);
         if (plan != null) {
             planRepository.delete(id);
             return id;
         } else {
             throw new IllegalArgumentException("선택한 일정은 존재하지 않습니다.");
+        }
+    }
+
+    public UserResponseDto join(UserRequestDto requestDto) {
+        User user = new User(requestDto);
+        User joinedUser = planRepository.join(user);
+        UserResponseDto userResponseDto = new UserResponseDto(joinedUser);
+        return userResponseDto;
+    }
+
+    public UserResponseDto login(UserRequestDto requestDto) {
+        String id = requestDto.getUserId();
+        String pw = requestDto.getPw();
+        User user = planRepository.identifyForUser(id, pw);
+
+        if (user != null) {
+            return planRepository.login(requestDto);
+        } else {
+            throw new IllegalArgumentException("ID와 PW를 확인하세요");
         }
     }
 }
